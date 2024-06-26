616b97737b8eacd78dd11858bd57936a611279dd
kanghowoo <kanghowoo@github.com>
2024-06-27 01:40:07 +0900

    refactor: change grouping multiple INSERT row (batch) to single INSERT with multiple VALUES (bulk query)

--- src/main/java/BulkInsertApp.java
+++ src/main/java/BulkInsertApp.java
@@ -1,8 +1,8 @@
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
@@ -14,6 +14,7 @@ public class BulkInsertApp {
 
     public static final int USER_ID = 1;
     public static final String APPLICATION_YML = "/application.yml";
+    public static final int BATCH_SIZE = 1000;
 
     @Parameter(names = {"--count"}, description = "Number of records to insert", required = true)
     private int count;
@@ -25,7 +26,10 @@ public class BulkInsertApp {
     }
 
     private void run() {
-        String insertSQL = "INSERT INTO boards"  + " (title, content, user_id) VALUES (?, ?, ?)";
+        if (count < 1) {
+            System.out.println("count should be bigger than 1");
+            System.exit(1);
+        }
 
         Map<String, Object> config = yamlConfigReader();
         Map<String, String> dataSource = (Map<String, String>) config.get("datasource");
@@ -35,23 +39,35 @@ public class BulkInsertApp {
 
         try (Connection connection = DriverManager.getConnection(url, user, password)) {
             connection.setAutoCommit(false);
-            try (PreparedStatement preparedStatement = connection.prepareStatement(insertSQL)) {
-                for (int i = 1; i <= count; i++) {
-                    preparedStatement.setString(1, "test_title" + i);
-                    preparedStatement.setString(2, "test_content" + i);
-                    preparedStatement.setInt(3, USER_ID);
-                    preparedStatement.addBatch();
 
-                    if (i % 1000 == 0 || i == count) {
-                        preparedStatement.executeBatch();
-                        connection.commit();
+            try (Statement statement = connection.createStatement()) {
+                for (int i = 1; i <= (count / BATCH_SIZE) + 1; i++) {
+                    StringBuilder sql = new StringBuilder("INSERT INTO boards (title, content, user_id) VALUES ");
+
+                    for (int j = 1; j <= BATCH_SIZE; j++) {
+                        sql.append("('")
+                           .append("test_title").append(i).append("', '")
+                           .append("test_content").append(i).append("', '")
+                           .append(USER_ID)
+                           .append("')");
+
+                        if (j < BATCH_SIZE) {
+                            sql.append(", ");
+                        }
                     }
+
+                    statement.executeUpdate(sql.toString());
                 }
+
+                connection.commit();
             } catch (SQLException e) {
+                System.out.println("sql statement error : " + e.getErrorCode());
                 connection.rollback();
+                System.exit(1);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            System.out.println("database connection error : " +e.getErrorCode());
+            System.exit(1);
         }
     }
 
